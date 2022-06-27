33d614aad5ecbabe7ff650f0a42c8dd901d211c9
spiegan77 <stephenpiegan@outlook.com>
2022-06-27 07:00:32 -0400

    RayCasting

---
+++ RPG_Course/Assets/Mover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    Ray lastRay;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
