b4e2b0e47ba294866f6128e4f6a3de054e856f20
spiegan77 <stephenpiegan@outlook.com>
2022-06-28 06:17:04 -0400

    Follow Camera

---
+++ RPG_Course/Assets/Scripts/Mover.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit == true)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+        //else { return; }
+    }
+
+    
+}
